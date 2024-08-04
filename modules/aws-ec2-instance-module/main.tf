fe8553a20565d402b6bf8cfdf5c83a46d002c970
Srinivasa Dara <srinivasa.dara@gmail.com>
2024-08-04 15:12:33 -0500

    Remove tags

--- modules/aws-ec2-instance-module/main.tf
+++ modules/aws-ec2-instance-module/main.tf
@@ -6,9 +6,4 @@
 resource "aws_instance" "dara_app_aws_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
-
-  #tags = {
-  #  instance_tags = var.instance_tags
-  #}
 }
-
