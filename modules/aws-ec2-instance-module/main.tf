417b71ec6394215105958e0a195f4d3e4ad6c483
Srinivasa Dara <srinivasa.dara@gmail.com>
2024-08-10 23:14:00 -0500

    Added EC2 tags

--- modules/aws-ec2-instance-module/main.tf
+++ modules/aws-ec2-instance-module/main.tf
@@ -1,9 +1,11 @@
 
-# provider "aws" {
-#  region = "us-west-2" # Change to your preferred region
-# }
+#  provider "aws" {
+#   region = var.aws_region
+#  }
+
 
 resource "aws_instance" "dara_app_aws_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  tags          = var.instance_tags
 }
